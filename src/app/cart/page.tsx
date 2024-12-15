10b51a9a2e52627428571cc3785f4d694c2d84eb
ABIHAAHMED4262 <abihaahmed413@gmail.com>
2024-12-15 15:11:45 +0500

    Changes in Cart

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -5,13 +5,13 @@ import DeliveryInformation from "@/components/DeliveryInformation";
 import Footer from "@/components/Footer";
 function cart() {
   return (
-    <div className="font-poppins">
+    <div className="font-poppins ">
   {/* Header */}
   <Header backgroundColor="#f8f9fa" />
 
   {/* Hero Section */}
   <div
-    className="relative w-full h-[316px] bg-cover bg-center"
+    className="relative w-full h-[316px] bg-cover bg-center "
     style={{ backgroundImage: "url(/images/shop.png)" }}
   >
     <div className="flex flex-col items-center justify-center h-full text-center">
@@ -30,12 +30,12 @@ function cart() {
   </div>
 
   {/* Main Content */}
-  <div className="grid grid-cols-1 md:grid-cols-[2fr_1fr] gap-6 mt-[50px] px-4 md:px-8 lg:px-16">
+  <div className="grid  md:grid-cols-2 sm:grid-cols-1  gap-9  mt-[50px] px-4 md:px-8 lg:px-16">
     {/* Products List Section */}
     <div>
       {/* Header Bar */}
       <div
-        className="hidden sm:flex justify-between items-center p-4 border-b border-gray-300"
+        className="md:w-[700px] hidden sm:flex justify-between items-center p-4 border-b border-gray-300"
         style={{ backgroundColor: "rgba(255, 249, 229, 1)" }}
       >
         <h3 className="w-1/3 text-center font-semibold text-black">Product</h3>
@@ -45,7 +45,7 @@ function cart() {
       </div>
 
       {/* Products */}
-      <div className="p-4">
+      <div className="p-4 md:w-[700px]">
         <div className="flex flex-wrap sm:flex-nowrap justify-between items-center mt-[39px] py-4 border-b border-gray-200">
           <Image
             src="/images/Asgaard.png"
@@ -77,11 +77,11 @@ function cart() {
 
     {/* Cart Totals Section */}
     <div
-      className="p-4 rounded-lg shadow-md"
+      className=" md:w-[400px] p-4 rounded-lg shadow-md md:ml-[150px]"
       style={{ backgroundColor: "rgba(255, 249, 229, 1)" }}
     >
       {/* Heading */}
-      <h3 className="text-2xl sm:text-4xl font-semibold text-black text-center mb-14">
+      <h3 className=" text-2xl sm:text-4xl font-semibold text-black text-center mb-14">
         Cart Totals
       </h3>
 
@@ -98,7 +98,7 @@ function cart() {
       </div>
 
       {/* Checkout Button */}
-      <button className="my-9 w-full h-[59px] text-black text-xl sm:text-2xl font-normal border border-black py-2 rounded-2xl hover:bg-blue-600 transition duration-200">
+      <button className="my-9 w-full h-[59px] text-black text-xl sm:text-2xl font-normal border border-black py-2 rounded-2xl hover:bg-black hover:text-white transition duration-200">
         Checkout
       </button>
     </div>
