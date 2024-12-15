6462d090daf5a947eaefec2bd0cdd1e0be1d5016
ABIHAAHMED4262 <abihaahmed413@gmail.com>
2024-12-15 17:17:11 +0500

    Changes in Header

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ const Hero: React.FC = () => {
   return (
     <div className="flex sm:flex-col md:flex-row lg:flex-row text-black bg-[rgba(251,235,181,1)] font-roboto gap-6 items-center ">
       {/* Left Section */}
-      <div className="flex flex-col md:pr-[60px] pl-[80px] justify-center items-start m-2 gap-2  lg:text-left  sm:text-center ">
+      <div className="flex flex-col md:pr-[60px] pl-[80px] justify-center items-start m-2 gap-2  lg:text-left md:text-center sm:text-center ">
         <h1 className=" font-medium  text-6xl md:text-5xl sm:h-[200px] lg:text-6xl sm:text-4xl ">
           Rocket single seater
         </h1>
