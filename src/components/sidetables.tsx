bada5bbc74fed959068d3bec72ddb35b52731cba
ABIHAAHMED4262 <abihaahmed413@gmail.com>
2024-12-16 15:18:35 +0500

    Changes in Sidetable component

--- src/components/sidetables.tsx
+++ src/components/sidetables.tsx
@@ -3,9 +3,11 @@ import Image from "next/image";
 
 function SideTables (){
     return(
-      <section className=" grid md:grid-cols-2  lg:grid-cols-2 font-poppins bg-gray-100 md:h-[700px]">
-        <div className="md:w-[500px] lg:w-[500px]">
-       <div className="flex flex-col md:h-[450]"><Image src="/images/table.png" alt="table.png" width={1092} height={100}/>
+      <section className=" grid grid-cols-1 md:grid-row-1 lg:grid-cols-2 font-poppins bg-gray-100 md:h-[700px]">
+        <div className="">
+       <div className="flex flex-col md:h-[450]">
+        <Image src="/images/table.png" alt="table.png" width={600}
+          height={853}/>
        <h1 className="text-black text-center text-4xl font-medium md:mb-6">Side table</h1>
        <a
             href="#"
@@ -16,8 +18,10 @@ function SideTables (){
        </div>
         </div>
 
-        <div className="md:w-[500px] lg:w-[500px]   ">
-        <div className="flex flex-col md:h-[450]"><Image src="/images/two seaters.png" alt="table.png" width={1092} height={100}/>
+        <div className="   ">
+        <div className="flex flex-col md:h-[450]">
+          <Image src="/images/two seaters.png" alt="table.png"  width={600}
+          height={853}/>
        <h1 className="text-black text-center text-4xl font-medium md:mb-6">Side table</h1>
        <a
             href="#"
